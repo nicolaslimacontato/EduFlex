29d2d594be46a7152ee53f1f962fa75c24619c1a
Nicolas <bigmacnls@gmail.com>
2024-08-09 10:59:26 -0300

    Mais recursos

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
     // Aplica a máscara de telefone
-    $('#phone').mask('(00) 00000-0000');
+    $('#phone').mask('(00) 00000-0000'); // Máscara com 5 dígitos
 
-    $('#contactPhoneInput').mask('(00) 00000-0000');
+    $('#contactPhoneInput').mask('(00) 00000-0000'); // Máscara com 5 dígitos
 });
+
 
 document.getElementById('contactForm').addEventListener('submit', function (event) {
     event.preventDefault();
@@ -28,12 +29,52 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     }
 
     // Validação do Telefone
-    const phonePattern = /^\(\d{2}\) \d{5}-\d{4}$/;
+    const phonePattern = /^\(\d{2}\) \d{4,5}-\d{4}$/; // Aceita 4 ou 5 dígitos antes do hífen
     if (!phonePattern.test(phone)) {
         alert('O telefone está no formato inválido.');
         isValid = false;
     }
 
+    // Validação da Forma de Contato Preferida
+    if (!preferredContactMethod) {
+        alert('A forma de contato preferida é obrigatória.');
+        isValid = false;
+    }
+
+    if (isValid) {
+        // Se todas as validações passarem, submeta o formulário
+        this.submit();
+    }
+});
+
+document.getElementById('homeForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const preferredContactMethod = document.querySelector('input[name="$PreferenciaContato"]:checked');
+
+    let isValid = true;
+
+    // Validação do Nome
+    if (!validator.isLength(name, { min: 1 })) {
+        alert('O nome é obrigatório.');
+        isValid = false;
+    }
+
+    // Validação do E-mail
+    if (!validator.isEmail(email)) {
+        alert('O e-mail é inválido.');
+        isValid = false;
+    }
+
+    // Validação do Telefone
+    const phonePattern = /^\(\d{2}\) \d{4,5}-\d{4}$/; // Aceita 4 ou 5 dígitos antes do hífen
+    if (!phonePattern.test(phone)) {
+        alert('O telefone está no formato inválido.');
+        isValid = false;
+    }
 
     // Validação da Forma de Contato Preferida
     if (!preferredContactMethod) {
@@ -49,7 +90,7 @@ document.getElementById('contactForm').addEventListener('submit', function (even
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper', {
+    var swiper1 = new Swiper('.swiper', {
         slidesPerView: 1,
         spaceBetween: 10,
         navigation: {
@@ -70,8 +111,94 @@ document.addEventListener('DOMContentLoaded', function () {
                 spaceBetween: 30,
             },
         },
+        autoplay: {
+            delay: 5000, // 5 segundos
+            disableOnInteraction: false,
+        },
+        on: {
+            slideChange: function () {
+                var slides = document.querySelectorAll('.swiper-slide');
+                slides.forEach(function (slide) {
+                    var cardImg = slide.querySelector('.card-img');
+                    if (cardImg) {
+                        cardImg.classList.remove('active'); // Remove a classe de sobreposição de todos os slides
+                    }
+                });
+
+                var activeSlide = document.querySelector('.swiper-slide-active');
+                var activeCardImg = activeSlide.querySelector('.card-img');
+                if (activeCardImg) {
+                    activeCardImg.classList.add('active'); // Adiciona a classe de sobreposição ao slide ativo
+                }
+            },
+        },
     });
+
+    var swiper2 = new Swiper('.swiper-products', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 5000, // 5 segundos
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        },
+        on: {
+            slideChange: function () {
+                var slides = document.querySelectorAll('.swiper-slide');
+                slides.forEach(function (slide) {
+                    var imageContainer = slide.querySelector('.image-container');
+                    if (imageContainer) {
+                        imageContainer.classList.remove('active'); // Remove a classe de sobreposição de todos os slides
+                    }
+                });
+
+                var activeSlide = document.querySelector('.swiper-slide-active');
+                var activeImageContainer = activeSlide.querySelector('.image-container');
+                if (activeImageContainer) {
+                    activeImageContainer.classList.add('active'); // Adiciona a classe de sobreposição ao slide ativo
+                }
+            },
+        },
+    });
+
+    // Verifica se é um dispositivo móvel
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        swiper2.on('slideChange', function () {
+            var slides = document.querySelectorAll('.swiper-slide');
+            slides.forEach(function (slide) {
+                var imageContainer = slide.querySelector('.image-container');
+                if (imageContainer) {
+                    imageContainer.classList.remove('active'); // Remove a classe de sobreposição de todos os slides
+                }
+            });
+
+            var activeSlide = document.querySelector('.swiper-slide-active');
+            var activeImageContainer = activeSlide.querySelector('.image-container');
+            if (activeImageContainer) {
+                activeImageContainer.classList.add('active'); // Adiciona a classe de sobreposição ao slide ativo
+            }
+        });
+    }
 });
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     function setEqualHeight() {
